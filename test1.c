b2cee932b72f27bde55730ffe62548f2cc3f6264
deepakreddy729 <deepakreddy91770@gmail.com>
2023-07-25 11:48:27 +0530

    Pushing Commit

---
+++ test1.c
@@ -0,0 +1,5 @@
+Hello guys, this is N Deepak Reddy, this document is to try a something on git commands which is to revert a pushed commands.
+Thank you,
+      Hello guys, this is N Deepak Reddy, this document is to try a something on git commands which is to revert a pushed commands.
+      Thank you,
+
