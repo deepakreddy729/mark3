d8f41777761f6c288ee31eaf42e075e6ffe4238c
deepakreddy729 <deepakreddy91770@gmail.com>
2023-07-25 12:04:36 +0530

    Second commit

--- test1.c
+++ test1.c
@@ -2,4 +2,5 @@ Hello guys, this is N Deepak Reddy, this document is to try a something on git c
 Thank you,
       Hello guys, this is N Deepak Reddy, this document is to try a something on git commands which is to revert a pushed commands.
       Thank you,
+This is my second commit that I want to revert using git revert. So lets see.
 
